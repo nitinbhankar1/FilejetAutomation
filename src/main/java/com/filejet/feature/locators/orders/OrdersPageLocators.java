3c81e9a30a69daa00d267718204ce01ad063d0f3
Nitin Bhankar <nbhankar@filejet.com>
2024-06-19 13:59:55 +0530

    xpath for the choose user and save changes

--- src/main/java/com/filejet/feature/locators/orders/OrdersPageLocators.java
+++ src/main/java/com/filejet/feature/locators/orders/OrdersPageLocators.java
@@ -8,5 +8,7 @@ public class OrdersPageLocators extends BaseHelp {
     public By orderPageLogo = By.xpath("//h4[@class='page-title']");
     public By takeActionButton = By.xpath("//button[@class='btn btn-secondary dropdown-toggle waves-effect waves-light'][1]");
     public By assignButton = By.xpath("(//a[@class='dropdown-item btn-assign'])[1]");
+    public By chooseUser = By.xpath(" (//select[@class='custom-select form-option1']//option[@value='2']");
+    public By savechangesButotn = By.xpath ("//button[@type='submit']");
 }
 
